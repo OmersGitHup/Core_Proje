848c7f2e53f0d7a0fe52fcfb685708e88b176dec
OmersGitHup <omerfaruk.isiq@gmail.com>
2024-07-29 15:58:59 +0300

    related tables added for dashboard and dashboard page edited.

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistic.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+	public class FeatureStatistic : ViewComponent
+	{
+		Context c=new Context();
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.v1 = c.Skills.Count();
+			ViewBag.v2= c.Messages.Where(x=>x.Status==false).Count();
+			ViewBag.v3= c.Testimonials.Count();
+			ViewBag.v4=c.Experiences.Count();
+			//var values = contactManager.TGetList();
+			return View();
+		}
+
+	}
+}
---
+++ Core_Proje/ViewComponents/Dashboard/StatisticsDashboard2.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class StatisticsDashboard2 : ViewComponent
+    {
+		Context c = new Context();
+
+		public IViewComponentResult Invoke()
+        {
+
+            ViewBag.numberOfProject = c.Porfolios.Count();
+            ViewBag.numberOfServices=c.Services.Count();
+            ViewBag.numberOfMessages=c.Messages.Count();
+
+            return View();
+        }
+
+
+    }
+}
