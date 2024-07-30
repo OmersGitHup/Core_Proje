feb3936e2a3b5be5bbcc28ed769b91e4e4df103a
OmersGitHup <omerfaruk.isiq@gmail.com>
2024-07-30 14:40:50 +0300

    Added areas for easy reading and added writer page for communication with admin . Started the using asp.netcore identity tools with app.

---
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Please Fill UserName")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Please Fill Password")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please Fill ConfirmPassword")]
+        [Compare("Password",ErrorMessage ="Passwords are NOT same !")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Please Fill Email")]
+        public string Email { get; set; }
+        
+    }
+}
