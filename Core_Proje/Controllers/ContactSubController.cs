b827b0c3e5b57e459856026f578217a9d8aa30ce
OmersGitHup <omerfaruk.isiq@gmail.com>
2024-08-11 10:41:36 +0300

    Added Testimonials and procejt section in app

---
+++ Core_Proje/Controllers/ContactSubController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ContactSubController : Controller
+    {
+        ContactManager _contactManager = new ContactManager(new EfContactDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _contactManager.TGetByID(2);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+
+            _contactManager.TUpdate(contact);
+            return RedirectToAction("Index", "Default");
+
+        }
+
+    }
+}
